ca65df64bf3499868060a6979d90e2b1cebda547
Anika <anikapatel120@gmail.com>
2020-04-26 08:30:26 -0700

    added backend, needs styling

--- public/index.js
+++ public/index.js
@@ -4,13 +4,13 @@ document.addEventListener("DOMContentLoaded", (event) => {
     var main = document.getElementById("main")
     
     button.addEventListener("click", (event) => {
-        fetch("http://api.openweathermap.org/data/2.5/weather?q=" + location.value + "&appid=904ecabe1bcaa06404f1c6fdc16861dc")
+        fetch("http://localhost:5000/weather/" + location.value)
         .then(data => {
             return data.json()
         })
         .then(json => {
             console.log(json)
-            main.innerHTML = json.name
+            main.innerHTML = ((json.data.main.temp) - 273.15) * 9/5 + 32
         })
 
         .catch(error => {
--- server/server.js
+++ server/server.js
@@ -6,6 +6,21 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '../public'), {index: false}))
 
+var apiKey = "904ecabe1bcaa06404f1c6fdc16861dc"
+
+app.get('/', function(request, response) {
+    response.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+app.get("/weather/:location", function(request, response) {
+    axios.get("http://api.openweathermap.org/data/2.5/weather?q=" + request.params.location + "&appid=904ecabe1bcaa06404f1c6fdc16861dc")
+    .then(data => {
+        console.log(data)
+        response.send({data: data.data})
+    })
+
+})
+
 app.listen("5000", function() { 
     console.log("Server running on 5000")
 })
